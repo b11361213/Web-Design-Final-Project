642a59a6c7382185c0229d4c42f49fdea3615911
b11361213 <114211720+b11361213@users.noreply.github.com>
2023-11-29 21:09:46 +0800

    1129 - fix signup.js error condition, resort signup.html forms

--- htdocs/scripts/signup.js
+++ htdocs/scripts/signup.js
@@ -10,40 +10,42 @@ function signup() {
     var birthday = document.getElementById('birthday').value;
 
     var AlertTitle = [
-        '⚠️ Username cannot be null',
-        '⚠️ Email cannot be null',
-        '⚠️ Password cannot be null',
-        '⚠️ Birthday cannot be null',
+        [
+            '⚠️ Username cannot be null',
+            '⚠️ Email cannot be null',
+            '⚠️ Birthday cannot be null',
+            '⚠️ Password cannot be null',
+        ],
         '⚠️ Invalid email format',
-        '⚠️ Password do not match',
-        '⚠️ Invalid date format'];
+        '⚠️ Invalid date format',
+        '⚠️ Password do not match'];
     var AlertContent = [
-        'Please fill out the field.', '', '',
+        'Please fill out the field.',
         'This is not a valid email format, Please try again.',
-        'Please make sure your passwords match',
-        'This is not a valid date format, Please try again.'];
+        'This is not a valid date format, Please try again.',
+        'Please make sure your passwords match'];
 
     if (username == '' || email == '' || passwd == '' || passwdchk == '' || birthday == '') {
-        if (birthday == '') { $('#AlertLabelTitle').text(AlertTitle[3]); }
-        if (passwd == '' || passwdchk == '') { $('#AlertLabelTitle').text(AlertTitle[2]); }
-        if (email == '') { $('#AlertLabelTitle').text(AlertTitle[1]); }
-        if (username == '') { $('#AlertLabelTitle').text(AlertTitle[0]); }
+        if (passwd == '' || passwdchk == '') { $('#AlertLabelTitle').text(AlertTitle[0][3]); }
+        if (birthday == '') { $('#AlertLabelTitle').text(AlertTitle[0][2]); }
+        if (email == '') { $('#AlertLabelTitle').text(AlertTitle[0][1]); }
+        if (username == '') { $('#AlertLabelTitle').text(AlertTitle[0][0]); }
         $('#AlertLabelContent').text(AlertContent[0]);
         $('#AlertLabel').modal('toggle');
     }
     else if (email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/) == null) {
-        $('#AlertLabelTitle').text(AlertTitle[3]);
-        $('#AlertLabelContent').text(AlertContent[3]);
+        $('#AlertLabelTitle').text(AlertTitle[1]);
+        $('#AlertLabelContent').text(AlertContent[1]);
+        $('#AlertLabel').modal('toggle');
+    }
+    else if (birthday.match(/(^1[9]\d{2}|2\d{3})\/(0[1-9]|1[012])\/(0[1-9]|[12][0-9]|3[01]$)|(^1[9]\d{2}|2\d{3})\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01]$)/) == null) {
+        $('#AlertLabelTitle').text(AlertTitle[2]);
+        $('#AlertLabelContent').text(AlertContent[2]);
         $('#AlertLabel').modal('toggle');
     }
     else if (passwd != passwdchk) {
-        $('#AlertLabelTitle').text(AlertTitle[4]);
-        $('#AlertLabelContent').text(AlertContent[4]);
-        $('#AlertLabel').modal('toggle');
-    }
-    else if (birthday.match(/(^2\d{3})\/(0[1-9]|1[012])\/(0[1-9]|[12][0-9]|3[01]$)|(^2\d{3})\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01]$)/) == null) {
-        $('#AlertLabelTitle').text(AlertTitle[5]);
-        $('#AlertLabelContent').text(AlertContent[5]);
+        $('#AlertLabelTitle').text(AlertTitle[3]);
+        $('#AlertLabelContent').text(AlertContent[3]);
         $('#AlertLabel').modal('toggle');
     }
     else {
